0a800407578b58e26192328e3f5facffca0dfa05
Jayson Brennan <jaysonbrennan@protonmail.com>
2023-07-02 14:21:21 -0400

    Improving button layout

--- Calculator/Views/OperationsView.swift
+++ Calculator/Views/OperationsView.swift
@@ -14,43 +14,32 @@ struct OperationsView: View {
                 .ignoresSafeArea()
             GeometryReader { geometry in
                 VStack {
-                    HStack(spacing: 8) {
-                        VStack(spacing: 8) {
-                            Grid {
-                                GridRow {
-                                    NumButton(number: 7)
-                                    NumButton(number: 8)
-                                    NumButton(number: 9)
-                                }
-                                GridRow {
-                                    NumButton(number: 4)
-                                    NumButton(number: 5)
-                                    NumButton(number: 6)
-                                }
-                                GridRow {
-                                    NumButton(number: 1)
-                                    NumButton(number: 2)
-                                    NumButton(number: 3)
-                                }
-                            }
-                            .frame(width: geometry.size.width * 0.75,
-                                   height: geometry.size.width * 0.75)
-                            NumButton(number: 0)
-                                .frame(width: geometry.size.width * 0.75,
-                                       height: geometry.size.width * 0.23)
-                        }
-                        VStack(spacing: 8) {
-                            OperatorButton(op: "÷")
-                            OperatorButton(op: "x")
-                            OperatorButton(op: "-")
-                            OperatorButton(op: "+")
-                        }
-                        .frame(width: geometry.size.width * 0.23,
-                               height: geometry.size.width)
+                    HStack {
+                        NumButton(number: 7)
+                        NumButton(number: 8)
+                        NumButton(number: 9)
+                        OperatorButton(op: "÷")
+                    }
+                    HStack {
+                        NumButton(number: 4)
+                        NumButton(number: 5)
+                        NumButton(number: 6)
+                        OperatorButton(op: "x")
+                    }
+                    HStack {
+                        NumButton(number: 1)
+                        NumButton(number: 2)
+                        NumButton(number: 3)
+                        OperatorButton(op: "-")
+                    }
+                    HStack {
+                        NumButton(number: 0)
+                            .frame(width: geometry.size.width * 0.75)
+                        OperatorButton(op: "+")
                     }
                     EqualButton()
-                        .frame(height: geometry.size.width * 0.23)
                 }
+                .frame(height: geometry.size.width * 1.2)
             }
             .padding(10)
         }
